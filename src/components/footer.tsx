063bc02a33f82d53e1dcd924900dfb3a17a10c65
Matt Kilcup <nevermindthelabel@gmail.com>
2020-07-26 13:54:24 -0700

    additional info in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>https://unsplash.com/@reallynattu</footer>;
+  return (
+    <footer>
+      <p>
+        Photo by <a href="https://unsplash.com/@reallynattu">Nattu Adnan</a> on
+        unsplash
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
