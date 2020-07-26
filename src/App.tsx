5a4b3b73f95501690c09e833073f7f6dbd90db05
Matt Kilcup <nevermindthelabel@gmail.com>
2020-07-26 13:32:49 -0700

    imported header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "../src/components/footer";
+import Header from "../src/components/header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h2>app works</h2>
       <Footer />
     </div>
