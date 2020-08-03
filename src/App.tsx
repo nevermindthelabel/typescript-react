c1a9bdc508675f1c032b06f9f5fc84974bcb82bd
Matt Kilcup <nevermindthelabel@gmail.com>
2020-08-02 20:35:05 -0700

    updated logic for main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Footer from "../src/components/footer";
 import Header from "../src/components/header";
 import QuestionCard from "./components/QuestionCard";
 import "./index.css";
 
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -21,7 +30,14 @@ const App = () => {
         </button>
         <p className="score"></p>
         <p>loading .....</p>
-        <QuestionCard />
+        <QuestionCard
+          questionNumber={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number]}
+          answers={questions[number]}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
         <button className="next" onClick={nextQuestion}>
           Next Question
         </button>
