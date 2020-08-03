6722e29430e61658f04f413dc35d587ed45a7f94
Matt Kilcup <nevermindthelabel@gmail.com>
2020-08-02 20:26:56 -0700

    updated question card layout and logic

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-const QuestionCard = () => {
-  return <div>Question</div>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className="number">
+        Question: {questionNumber} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }}></p>
+      <div>
+        {answers.map((answer) => (
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default QuestionCard;
