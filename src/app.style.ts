69352df7620ea18e91c676dda0b61d59421faa6f
Matt Kilcup <nevermindthelabel@gmail.com>
2021-04-18 19:24:35 -0700

    updated css properties

--- src/app.style.ts
+++ src/app.style.ts
@@ -13,20 +13,20 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     padding: 0 20px;
-    place-items: center;
 }
 body {
     margin: 0;
     padding: 0;
 }
 main {
+    display: grid;
     grid-column: 1 / -1;
+    place-items: start center;
 }
 header {
     display: grid;
     place-items: center center;
     grid-column: 1 / -1;
-    color: pink;
 }
 footer {
     display: grid;
@@ -39,3 +39,44 @@ footer {
     font-family: 'Catamaran', sans-serif;
 }
 `;
+
+export const Wrapper = styled.div`
+    display: grid;
+    place-items: center;
+  > p {
+    color: #ffffff;
+  }
+  .score {
+      #ffffff;
+      font-size: 2rem;
+      margin: 0
+  }
+  h2 {
+      font-family: Fascinate Inline;
+      background-image: linear-gradient(180deg, #ffffff, #87f1ff);
+      background-size: 100%;
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      -moz-background-clip: text;
+      -moz-text-fill-color: transparent;
+      filter: drop-shadow(2px 2px #0085a3);
+      font-size: 70px;
+      font-weight: 400;
+      text-align: center;
+      margin: 20px
+      }
+      .start, .next {
+          cursor: pointer;
+          background: linear-gradient(180deg, #ffffff, #ffcc91);
+          border: 2px solid #d38558;
+          box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+          border-radius: 10px;
+          height: 40px;
+          margin: 20px 0;
+          padding: 0 40px;
+      }
+      .start {
+          max-width: 200px;
+      }
+      `;
